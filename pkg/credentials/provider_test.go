c85f8a20f53adf01e11f33dced8d97c8c955f56b
Michael Montgomery <77761476+mmontgomery-square@users.noreply.github.com>
2021-02-22 09:42:45 -0800

    Increasing test coverage (#8)

--- pkg/credentials/provider_test.go
+++ pkg/credentials/provider_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/square/spiffe-aws-assume-role/internal/mocks"
 	"github.com/square/spiffe-aws-assume-role/internal/test"
@@ -49,6 +50,79 @@ func TestNewProviderAssignsSessionDuration(t *testing.T) {
 	provider, err := NewProvider(audience, roleArn, jwtSource, nonZeroSessionDuration, stsProvider)
 	require.NoError(t, err)
 	require.Equal(t, nonZeroSessionDuration, provider.SessionDuration)
+}
+
+func TestRetrieveSetsExpirationOnCredentials(t *testing.T) {
+	var audience, roleArn string
+	var sessionDuration = time.Second
+
+	jwtSource := mocks.JWTSource{}
+	defer jwtSource.AssertExpectations(t)
+	jwtSource.
+		On("FetchToken", mock.Anything).
+		Return("token", nil)
+
+	expiration := time.Now()
+	copyOfExpiration := expiration
+
+	credentials := sts.Credentials{
+		AccessKeyId:     aws.String(""),
+		Expiration:      &copyOfExpiration,
+		SecretAccessKey: aws.String(""),
+		SessionToken:    aws.String(""),
+	}
+
+	assumeRoleResponse := sts.AssumeRoleWithWebIdentityOutput{
+		Credentials: &credentials,
+	}
+
+	stsClient := mocks.STSAPI{}
+	defer stsClient.AssertExpectations(t)
+	stsClient.
+		On("AssumeRoleWithWebIdentityWithContext", mock.Anything, mock.Anything).
+		Return(&assumeRoleResponse, nil)
+
+	stsProvider := StaticSTSProvider(&stsClient)
+
+	provider, err := NewProvider(audience, roleArn, &jwtSource, sessionDuration, stsProvider)
+	require.NoError(t, err)
+
+	_, err = provider.Retrieve()
+	require.NoError(t, err)
+
+	require.EqualValues(t,
+		expiration.Round(0).Add(-time.Minute),
+		provider.Expiry.ExpiresAt())
+}
+
+func TestAssumeRoleAppendsPolicies(t *testing.T) {
+	stsClient := mocks.STSAPI{}
+	defer stsClient.AssertExpectations(t)
+
+	captor := test.Captor{}
+
+	stsClient.
+		On("AssumeRoleWithWebIdentityWithContext", mock.Anything, mock.Anything).
+		Run(captor.Capture()).
+		Return(&sts.AssumeRoleWithWebIdentityOutput{}, nil)
+
+	policy := "foo"
+	policyArn1 := "bar"
+	policyArn2 := "baz"
+
+	provider := Provider{
+		Policy:     policy,
+		PolicyARNs: []string{policyArn1, policyArn2},
+		stsClient:  &stsClient,
+	}
+	_, err := provider.assumeRole(context.Background(), "role")
+	require.NoError(t, err)
+
+	input := captor.Args.Get(1).(*sts.AssumeRoleWithWebIdentityInput)
+	require.EqualValues(t, policy, *input.Policy)
+
+	require.EqualValues(t, policyArn1, *input.PolicyArns[0].Arn)
+	require.EqualValues(t, policyArn2, *input.PolicyArns[1].Arn)
 }
 
 func randomInt32(maxExclusive int32) int32 {
